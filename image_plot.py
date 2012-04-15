9ff07560e0aa8ff602b7b553566fbd91ba545be5
Kelsey Jordahl <kjordahl@enthought.com>
2012-04-15 14:32:57 -0400

    Axis labels in km

--- image_plot.py
+++ image_plot.py
@@ -1,5 +1,5 @@
 from traits.api import HasTraits, Instance
-from chaco.api import Plot, ArrayPlotData, TransformColorMapper, jet
+from chaco.api import Plot, ArrayPlotData, TransformColorMapper, PlotAxis, jet
 
 
 class ImagePlot(HasTraits):
@@ -26,7 +26,22 @@ class ImagePlot(HasTraits):
         self.plot.request_redraw()
 
     def get_plot_component(self):
+        xbounds = self.model.xh[0] / 1000, self.model.xh[-1] / 1000
+        ybounds = self.model.yh[0] / 1000, self.model.yh[-1] / 1000
         self.plotdata.set_data("imagedata", self.model.Z)
         tcm = TransformColorMapper.from_color_map(jet)
-        renderer = self.plot.img_plot("imagedata", colormap=tcm)[0]
+        renderer = self.plot.img_plot("imagedata",
+                                      xbounds = xbounds,
+                                      ybounds = ybounds,
+                                      colormap=tcm)[0]
+        left = PlotAxis(orientation='left',
+                        title='km',
+                        mapper=self.plot.value_mapper,
+                        component=self.plot)
+        bottom = PlotAxis(orientation='bottom',
+                        title='km',
+                        mapper=self.plot.value_mapper,
+                        component=self.plot)
+        self.plot.underlays.append(left)
+        self.plot.underlays.append(bottom)
         return self.plot
